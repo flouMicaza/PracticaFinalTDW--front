6d58dd6302058c93b3f71f308fb4afe8bc94fc5d
Florencia Miranda <flo.mirandai@gmail.com>
2019-05-22 00:12:21 +0200

    Login funciona

--- js/login.js
+++ js/login.js
@@ -1,29 +1,76 @@
+function inicioLogin() {
+  $("#botonLogin").click(function() {
+    var nombre = $("#user").val();
+    var contrasena = $("#contrasena").val();
+    console.log("holaaa", nombre, " ", contrasena);
+    $.post(
+      "http://localhost:8000/api/v1/login",
+      {
+        _username: nombre,
+        _password: contrasena
+      },
+      function(data, status) {
+        console.log(status);
+        if (status == "success") {
+          location.href = "inicio.html";
+        } else {
+          alert("fallo");
+        }
+      }
+    );
+  });
+}
+
+function prueba() {
+  console.log(httpRequest.status, "golaaa");
+  var mi_div = document.createElement("div");
+  var mi_small = document.createElement("small");
+  var mensaje = document.createTextNode("Credenciales incorrectas");
+  mi_small.appendChild(mensaje);
+  mi_div.appendChild(mi_small);
+  var form = document.getElementById("login");
+  alert(httpRequest.status, "respuesta");
+  form.insertBefore(mi_div, form.childNodes[5]);
+  if (httpRequest.status == 200) {
+    alert(httpRequest, "respuesta");
+  } else {
+    alert("holaa");
+  }
+}
+
 function validacion() {
   //trae todos los datos
-  var datos = JSON.parse(window.localStorage.getItem("datos"));
+  //var datos = JSON.parse(window.localStorage.getItem("datos"));
   //trae el user y contraseña
   var nombre = document.getElementById("user").value;
   var contrasena = document.getElementById("contrasena").value;
+  console.log(nombre + contrasena);
+  datos = { _username: nombre, _password: contrasena };
+  httpRequest = new XMLHttpRequest(); //para hacer ajax
+  httpRequest.open("POST", "http://localhost:8000/api/v1/login", true);
+  httpRequest.responseType = "json";
+  httpRequest.onload = prueba; //cuando se carguen los datos llama a esta funcion.
+  httpRequest.send("_username=admin&_password=admin");
+  // var usuario = getUsuario(datos, nombre, contrasena);
 
-  var usuario = getUsuario(datos, nombre, contrasena);
+  // if (usuario == null) {
+  //   nombre.value = "";
+  //   contrasena.value = "";
+  var mi_div = document.createElement("div");
+  var mi_small = document.createElement("small");
+  var mensaje = document.createTextNode("Credenciales incorrectas");
+  mi_small.appendChild(mensaje);
+  mi_div.appendChild(mi_small);
+  var form = document.getElementById("login");
 
-  if (usuario == null) {
-    nombre.value = "";
-    contrasena.value = "";
-    var mi_div = document.createElement("div");
-    var mi_small = document.createElement("small");
-    var mensaje = document.createTextNode("Credenciales incorrectas");
-    mi_small.appendChild(mensaje);
-    mi_div.appendChild(mi_small);
-    var form = document.getElementById("login");
-    form.insertBefore(mi_div, form.childNodes[5]);
-  } else {
-    window.localStorage.setItem("usuarioRegistrado", JSON.stringify(usuario));
-    var login = document.getElementById("login");
-    login.action = "./inicio.html";
-  }
+  form.insertBefore(mi_div, form.childNodes[5]);
+  // } else {
+  //   window.localStorage.setItem("usuarioRegistrado", JSON.stringify(usuario));
+  var login = document.getElementById("login");
+  login.action = "./login.html";
+  // }
 
-  return usuario != null;
+  // return usuario != null;
 }
 
 function getUsuario(datos, nombre, contrasena) {
